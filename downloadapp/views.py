0fd3bea3135201bd6d79ea4cf61ee2ea83821eeb
Sourcery AI <>
2022-07-08 04:28:27 +0000

    'Refactored by Sourcery'

--- downloadapp/views.py
+++ downloadapp/views.py
@@ -8,21 +8,20 @@ from django.http.response import HttpResponse
 
 
 def download_file(request, filename='/last_cv.pdf'):
-    if filename != '':
-        # Define Django project base directory
-        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        # Define the full file path
-        filepath = BASE_DIR + filename
-        # Open the file for reading content
-        path = open(filepath, 'rb')
-        # Set the mime type
-        mime_type, _ = mimetypes.guess_type(filepath)
-        # Set the return value of the HttpResponse
-        response = HttpResponse(path, content_type=mime_type)
-        # Set the HTTP header for sending to browser
-        response['Content-Disposition'] = "attachment; filename=%s" % 'my_cv.pdf'
-        # Return the response value
-        return response
-    else:
+    if filename == '':
         # Load the template
         return render(request, 'Halem/cv.html')
+    # Define Django project base directory
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    # Define the full file path
+    filepath = BASE_DIR + filename
+    # Open the file for reading content
+    path = open(filepath, 'rb')
+    # Set the mime type
+    mime_type, _ = mimetypes.guess_type(filepath)
+    # Set the return value of the HttpResponse
+    response = HttpResponse(path, content_type=mime_type)
+        # Set the HTTP header for sending to browser
+    response['Content-Disposition'] = 'attachment; filename=my_cv.pdf'
+    # Return the response value
+    return response
